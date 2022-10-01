c8566525bf980f078ac0a5dacaf1274c936b6a08
Cari Costa <cari.costa90@gmail.com>
2022-10-01 11:27:26 -0300

    Updates

---
+++ precache-manifest.1b246bf68b854b9e851053bab10d4759.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae63abd230f0bbbf2b94",
+    "url": "/tiska_shop_Carina_Costa/static/css/main.48d75ca2.chunk.css"
+  },
+  {
+    "revision": "ae63abd230f0bbbf2b94",
+    "url": "/tiska_shop_Carina_Costa/static/js/main.ae63abd2.chunk.js"
+  },
+  {
+    "revision": "79d3298a13e547ff2833",
+    "url": "/tiska_shop_Carina_Costa/static/js/1.79d3298a.chunk.js"
+  },
+  {
+    "revision": "ea04057d21a2e7a2121b",
+    "url": "/tiska_shop_Carina_Costa/static/js/runtime~main.ea04057d.js"
+  },
+  {
+    "revision": "bfc2eb5d9fcb6900d22e2347378f7c29",
+    "url": "/tiska_shop_Carina_Costa/static/media/tiska.bfc2eb5d.gif"
+  },
+  {
+    "revision": "acf705bea5f324329aca07b14ba19fff",
+    "url": "/tiska_shop_Carina_Costa/static/media/header_background_1.acf705be.jpg"
+  },
+  {
+    "revision": "ec7136b1448b50eb84e5e36c47d8622b",
+    "url": "/tiska_shop_Carina_Costa/index.html"
+  }
+];
