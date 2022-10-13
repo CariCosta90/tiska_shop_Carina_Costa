13386aff344ff88646285917cf18a00d2338522a
Cari Costa <cari.costa90@gmail.com>
2022-10-13 18:59:56 -0300

    correccion de promesas

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { ItemCount } from "../ItemCount/ItemCount";
 import ItemList from "../ItemList/ItemList";
 import Skeleton from '@mui/material/Skeleton';
 
